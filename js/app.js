5f3516e1d180e573953aef16ce3cd2385c48525d
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2021-01-05 11:25:55 +0530

    modified

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 const checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="11" height="8"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>';
-const circle = document.querySelectorAll(".circle");
 const list = document.querySelector(".list_items");
+const data = document.querySelector(".data");
 const input_field = document.querySelector("#todo_items");
-let check = true;
+const circle = document.querySelectorAll(".circle");
+
+input_field.addEventListener("keyup", e=>{
+    if(e.key === "Enter"){
+        const data_element = document.createElement("td");
+        const circle_element = document.createElement("div")
+        circle_element.className = "circle";
+        data_element.append(circle_element);
+        data_element.append(input_field.value);
+        data.appendChild(data_element);
+        input_field.value = "";
+    }
+})
 circle.forEach(round_obj =>{
     round_obj.addEventListener("click", e=>{
         if(!round_obj.classList.contains("selector")){
@@ -13,10 +25,4 @@ circle.forEach(round_obj =>{
             round_obj.innerHTML = '';
         }
     })
-})
-input_field.addEventListener("keyup", e=>{
-    if(e.key === "Enter"){
-        console.log(input_field.value);
-        input_field.value = "";
-    }
 })
