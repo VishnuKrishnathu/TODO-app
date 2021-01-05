b3bbc210a0fb6329bdfed62bc955b86225a7b095
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2021-01-05 10:29:44 +0530

    modified

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 const checkmark = '<svg xmlns="http://www.w3.org/2000/svg" width="11" height="8"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>';
 const circle = document.querySelectorAll(".circle");
+const list = document.querySelector(".list_items");
+const input_field = document.querySelector("#todo_items");
 let check = true;
 circle.forEach(round_obj =>{
     round_obj.addEventListener("click", e=>{
-        if(check === true){
+        if(!round_obj.classList.contains("selector")){
+            round_obj.classList.add("selector");
             round_obj.innerHTML = checkmark;
-            check= false;
         }else{
-            round_obj.innerHTML ="";
-            check= true;
+            round_obj.classList.remove("selector");
+            round_obj.innerHTML = '';
         }
     })
+})
+input_field.addEventListener("keyup", e=>{
+    if(e.key === "Enter"){
+        console.log(input_field.value);
+        input_field.value = "";
+    }
 })
